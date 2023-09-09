647b5ec2e85cd58e16367885687d908a325b941b
vitormoscoso <vitormoscoso@gmail.com>
2023-09-09 15:01:46 -0300

    🎨

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,43 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { Text, View, RefreshControl, ScrollView } from "react-native";
-import { auth } from "../../firebase/config";
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import { Card } from "react-native-paper";
+import Icon from "react-native-vector-icons/Feather";
 import Header from "../../components/Header/header";
-import { getData } from "../../services/apiClient";
+import { auth } from "../../firebase/config";
 
 export default function HomeScreen({ navigation }) {
-  const user = auth.currentUser;
-  const [data, setData] = useState([]);
-  const [refreshing, setRefreshing] = useState(false);
+  // const user = auth.currentUser;
 
   function logout() {
     auth.signOut();
     navigation.replace("Login");
   }
 
-  async function getDataTest() {
-    setRefreshing(true);
-    const data = await getData(setData);
-    setData(data);
-    setRefreshing(false);
-  }
-
-  useEffect(() => {
-    getDataTest();
-  }, []);
-
   return (
     <>
       <Header logout={logout} />
-      <Text style={{ marginLeft: "5%" }}>Bem-vindo, {user.email}!</Text>
-      <ScrollView style={{ margin: "5%" }}>
-        {data?.map((el) => {
-          return (
-            <View key={el.id} style={{ marginBottom: "5%" }}>
-              <Text>Nível: {el.value} cm</Text>
-              <Text>Horário: {el.timestamp}</Text>
+      {/* <Text style={{ marginLeft: "5%" }}>Bem-vindo, {user.email}!</Text> */}
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "5%",
+            marginBottom: "5%",
+          }}
+        >
+          <Card
+            style={{
+              width: "90%",
+              backgroundColor: "#E1E5E6",
+              marginBottom: "3%",
+            }}
+          >
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginTop: "3%",
+                marginBottom: "5%",
+                marginLeft: "5%",
+                marginRight: "5%",
+              }}
+            >
+              <Text style={{ fontSize: 15 }}>Nível atual</Text>
+              <Icon
+                name={"more-horizontal"}
+                size={25}
+                color="black"
+                onPress={() => navigation.navigate("Historico de medições")}
+              />
             </View>
-          );
-        })}
+            <Card.Content style={{ alignItems: "center" }}>
+              <Text
+                style={{ fontWeight: "bold", fontSize: 20, marginBottom: "2%" }}
+              >
+                1,2 m
+              </Text>
+              <Text>08/09 - 13:15:44</Text>
+            </Card.Content>
+          </Card>
+          <Card
+            style={{
+              width: "90%",
+              backgroundColor: "#E1E5E6",
+              marginBottom: "3%",
+            }}
+          >
+            <Card.Title title="Capacidade" />
+            <Card.Content style={{ alignItems: "center" }}>
+              <Text
+                style={{ fontWeight: "bold", fontSize: 20, marginBottom: "2%" }}
+              >
+                20%
+              </Text>
+              <Text>disponível</Text>
+            </Card.Content>
+          </Card>
+        </View>
       </ScrollView>
     </>
   );
--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -6,27 +6,44 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { db } from "../firebase/config";
+import { formatDate } from "../utils/date";
 
 export async function getData(setData) {
-  const q = query(collection(db, "level"));
-  onSnapshot(q, (querySnapshot) => {
-    const responseData = [];
-    querySnapshot.forEach((doc) => {
-      responseData.push({ id: doc.id, ...doc.data() });
+  try {
+    const q = query(collection(db, "level"));
+    onSnapshot(q, (querySnapshot) => {
+      const responseData = [];
+      querySnapshot.forEach((doc) => {
+        responseData.push({
+          id: doc.id,
+          timestamp: formatDate(doc.data().timestamp),
+          value: doc.data().value,
+        });
+      });
+      setData(responseData);
     });
-    setData(responseData);
-  });
-  //   try {
-  //     const querySnapshot = await getDocs(collection(db, "level"));
-  //     let responseData = [];
-
-  //     querySnapshot.forEach((doc) => {
-  //       let data = { id: doc.id, ...doc.data() };
-  //       responseData.push(data);
-  //     });
-
-  //     return responseData;
-  //   } catch (error) {
-  //     console.log("erro: ", error);
-  //   }
+  } catch (error) {
+    console.log("erro: ", error);
+  }
 }
+
+// export async function getCurrentData(setCurrentData) {
+//   try {
+//     const q = query(
+//       collection(db, "level"),
+//       orderBy("timestamp", "desc"),
+//       limit(1)
+//     );
+//     onSnapshot(q, (querySnapshot) => {
+//       querySnapshot.forEach((doc) => {
+//         setCurrentData({
+//           id: doc.id,
+//           timestamp: formatDate(doc.data().timestamp),
+//           value: doc.data().value,
+//         });
+//       });
+//     });
+//   } catch (error) {
+//     console.log("erro: ", error);
+//   }
+// }
